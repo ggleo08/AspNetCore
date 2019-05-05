712914aee382fa48f7e19822341db70c357a3859
stafford <stafford.williams@gmail.com>
2019-05-05 17:37:09 +1000

    increase defaults

--- src/SignalR/perf/benchmarkapps/Crankier/Commands/Defaults.cs
+++ src/SignalR/perf/benchmarkapps/Crankier/Commands/Defaults.cs
@@ -7,8 +7,8 @@ namespace Microsoft.AspNetCore.SignalR.Crankier.Commands
 {
     internal static class Defaults
     {
-        public static readonly int NumberOfWorkers = 1;
-        public static readonly int NumberOfConnections = 10_000;
+        public static readonly int NumberOfWorkers = 10;
+        public static readonly int NumberOfConnections = 50_000;
         public static readonly int SendDurationInSeconds = 300;
         public static readonly HttpTransportType TransportType = HttpTransportType.WebSockets;
     }
